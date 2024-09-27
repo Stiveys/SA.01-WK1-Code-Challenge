376fbc106178e5a26cd7c85d3738403924694d25
Stiveys <stevomwaura@gmail.com>
2024-09-27 15:23:24 +0300

    new

---
+++ grading-assignment.js
@@ -0,0 +1,17 @@
+function calculateGrade(marks) {
+if (marks > 79) {
+    return 'A';
+}
+else if (marks >= 60) {
+    return 'B';
+}
+else if (marks >= 49) {
+    return 'C';
+}
+else if (marks >= 40) {
+    return 'D';
+}
+else {
+    return 'E';
+}
+}
